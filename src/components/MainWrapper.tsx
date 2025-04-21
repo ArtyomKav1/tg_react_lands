e8bbd7a8d9e0c8f28f9a15f3e57810585d82f311
Artyom K <artem.kavun90@gmail.com>
2025-04-21 15:02:13 +0300

    5

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -23,11 +23,14 @@ function MainWrapper() {
     };
 
     return (
-        <div>
-            <p>Это приложение запущено внутри Telegram!</p>
-            <p>Init Data: {tg.initData}</p>
-            <p>User: {tg.initDataUnsafe?.user}</p>
-            <button onClick={closeWebApp}>Закрыть приложение</button>
+        <div className="flex flex-col justify-center">
+            <p className="text-[32px] text-center ">Это приложение запущено внутри Telegram!</p>
+            <div className="flex flex-col border rounded-[8px] bg-amber-100">
+                <p>Init Data: {tg.initData}</p>
+                <p>User: {tg.initDataUnsafe?.user}</p>
+                <div>{tg.map((item) => <div>{item}</div>)}</div>
+            </div>
+            <button onClick={closeWebApp} className="py-[10px] px-[20px] bg-amber-400">Закрыть приложение</button>
         </div>
     );
 }
